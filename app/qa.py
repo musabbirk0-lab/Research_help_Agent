6a6aaadfc4af6a4dc2d8acba88a24e41cc2754ff
Musabbir Ahmed Khan <musabbirk0@gmail.com>
2025-11-24 00:45:27 -0800

    Update qa.py

--- app/qa.py
+++ app/qa.py
@@ -1,1 +1,8 @@
+import numpy as np
+from app.ingest import EMBED_MODEL, create_or_load_index
 
+def retrieve(query: str, k=3):
+    vec = EMBED_MODEL.encode([query], convert_to_numpy=True)
+    index = create_or_load_index(vec.shape[1])
+    D, I = index.search(vec, k)
+    return I[0].tolist()
